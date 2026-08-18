def9df82c628a6bdf4e0507b7d58300c97d7bddc
Durvang <durvangchavan23@gmail.com>
2026-08-18 09:32:53 +0530

    add code for basic-react.

---
+++ 01_basic-react/src/Alien.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Alien() {
+  return (
+    <div>
+        <h1>This is Alien component!</h1>
+        <h3>Hello, World!</h3>
+    </div>
+  )
+}
+
+export default Alien
